056600031ef51f59b68ecb655ab22471de8c9300
ysicing <i@ysicing.me>
2020-07-02 10:03:52 +0800

    improvement: update typo

--- delete.sh
+++ delete.sh
@@ -6,6 +6,7 @@ kubectl delete -f node-exporter
 kubectl delete -f kube-state-metrics
 kubectl delete -f alertmanager
 kubectl delete -f grafana
+kubectl delete -f blackbox-exporter
 kubectl delete -f prometheus-service
 kubectl delete -f setup
 kubectl delete -f ingress
